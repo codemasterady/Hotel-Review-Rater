633286f6ec0718ed4523179a2d1629d5fce5e419
Aditya Sivadas <62368349+codemasterady@users.noreply.github.com>
2020-10-23 18:31:03 +0800

    Add files via upload

--- Ratings Predictor/main.py
+++ Ratings Predictor/main.py
@@ -1,6 +1,9 @@
 # Importing the libraries
 from screens.main_gui import GraphicalUserInterface
+from models.training_model import NeuralEngine
 
-# Calling the Graphical User Imterface
-obj = GraphicalUserInterface()
-obj.generateMainGUI()
+# Testing the model
+review = "Guests liked the clean, updated rooms, though some said they were small & maintenance could be improved · Rooms had views · Some guests said the bathrooms were small & cleanliness could be improved"
+engine = NeuralEngine()
+engine.classify_train()
+engine.predict_classifier(review)
--- Ratings Predictor/models/training_model.py
+++ Ratings Predictor/models/training_model.py
@@ -1,45 +1,78 @@
-from nltk.stem.porter import PorterStemmer
-from nltk.corpus import stopwords
+# Importing the libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
 import nltk
-# nltk.download('stopwords')
-dataset = pd.read_csv("tripadvisor_hotel_reviews.csv")
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.naive_bayes import GaussianNB
+# Downloading stopwords
+nltk.download('stopwords')
 
-class trainer:
-    def __init__(self, dataset):
-        self.dataset = dataset
+
+class NeuralEngine:
+    def __init__(self):
+        # Getting the DataSet
+        self.nb = GaussianNB()
         pass
-    
-    #! Converts text dataset into Bag Of Words
-    def text_to_array(self, max_lim =47000):
+
+    #! Training & Creating the Classifier model
+    def classify_train(self):
+        dataset = pd.read_csv(
+            r"C:\Users\Selvaseetha\Repository\Ratings Predictor\models\tripadvisor_hotel_reviews.csv").values
+        # Cleaning The Text
         corpus = []
-        for i in range(0, 20000):
-                    review = re.sub("^a-zA-Z", ' ', self.dataset["Review"][i])
-                    review = review.lower()
-                    review = review.split()
-                    ps = PorterStemmer()
-                    all_stopwords = stopwords.words('english')
-                    all_stopwords.remove('not')
-                    review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
-                    review = " ".join(review)
-                    corpus.append(review)  
-        from sklearn.feature_extraction.text import CountVectorizer
-        cv = CountVectorizer(max_features= max_lim)
-        X = cv.fit_transform(corpus).toarray()
-        print(len(X[0]))
-        return np.array(X).reshape(-1, 1)
-    
-    #! Uses the Naive-Bayes Classifier
-    def classifier(self):
-        pass
-    
-    #! Trains a neural network
-    def train_model(self):
-        pass
-        
-        
+        for i in range(0, 10000):
+            review = re.sub("^a-zA-Z", ' ', dataset[i, 0])
+            review = review.lower()
+            review = review.split()
+            ps = PorterStemmer()
+            all_stopwords = stopwords.words('english')
+            all_stopwords.remove('not')  # Ignores the not value
+            review = [ps.stem(word)
+                      for word in review if not word in set(all_stopwords)]
+            review = " ".join(review)
+            corpus.append(review)
 
-      
+        # Implementing the Bag Of Words Model (Count Vectorizer)
+
+        from sklearn.feature_extraction.text import CountVectorizer
+        cv = CountVectorizer(max_features=47000)
+        X = cv.fit_transform(corpus).toarray()
+
+        # Getting the dependant variable
+
+        y = dataset[0:10000, 1]
+
+        # Splitting the data into the training & test set
+
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.1)
+
+        # Using Naive Bayes to train
+        y = y.astype('int')  # ! To solidify the type
+        y = y.reshape(-1, 1)
+        self.nb.fit(X, np.array(y))
+        pass
+
+    #! Predicting the final Outputs
+    def predict_classifier(self, text):
+        review = re.sub("^a-zA-Z", ' ', text)
+        review = review.lower()
+        review = review.split()
+        ps = PorterStemmer()
+        all_stopwords = stopwords.words('english')
+        all_stopwords.remove('not')  # Ignores the not value
+        review = [ps.stem(word)
+                  for word in review if not word in set(all_stopwords)]
+        review = " ".join(review)
+        review = np.array(review).reshape(-1, 1)
+        from sklearn.feature_extraction.text import CountVectorizer
+        cv = CountVectorizer(max_features=47000)
+        X = cv.fit_transform(review).toarray()
+        X = np.array(X).reshape(-1, 1)
+        y_pred = self.nb.predict(X)
+        print(y_pred)
+        pass
